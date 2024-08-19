f386eb30208c2c45df5e5acf942f479e9ae149a8
muhammadsarimwaseem <sarimwaseem84@gmail.com>
2024-08-19 23:29:57 +0500

    updated

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.set("views", path.resolve('views'))
 //Middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: 'false' }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(cookieParser())
 
 //routes
